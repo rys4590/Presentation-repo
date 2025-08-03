3a809f1b3fa592b7ad38a9cc73236fc6bf6b42d3
rys4590 <ronnie.yadid@gmail.com>
2025-08-03 17:38:57 +0300

    Question5

--- Student.java
+++ Student.java
@@ -1,15 +1,16 @@
-public class Student {
-    public record student(String name,int grade, int classNum){
-        public boolean isHighSchool(){
-            return(this.grade>9);
-        }
 
-        public String toString(){
-            return "grade:"+this.grade+" name:"+this.name()+" classNumber:" + this.classNum;
-        }
-
-        public Student(int grade, String name){
-            this.name =
-        }
+public record Student(String name,int grade, int classNum){
+    public boolean isHighSchool(){
+        return(this.grade>9);
     }
+
+    public String toString(){
+        return "grade:"+this.grade+" name:"+this.name()+" classNumber:" + this.classNum;
+    }
+
+    public Student(int classNum, String name){
+        this(name,9,classNum);
+    }
+
 }
+
