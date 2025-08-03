bdb2d1b39a7c8716e42a885b91514ef40fbf8b36
rys4590 <ronnie.yadid@gmail.com>
2025-08-03 16:34:31 +0300

    Questions1-5

--- Bed.java
+++ Bed.java
@@ -3,11 +3,14 @@ public class Bed {
     private double height;
     private int numPillows;
     private String sheetColor;
+    private double price;
 
-    public Bed(double height, int numPillows, String sheetColor){
+
+    public Bed(double height, int numPillows, String sheetColor, double price){
         this.numPillows = numPillows;
         this.height = height;
         this.sheetColor = sheetColor;
+        this.price=price;
     }
 
     public double getHeight() {
@@ -21,6 +24,18 @@ public class Bed {
     public String getSheetColor() {
         return sheetColor;
     }
+
+    public Bed(double height, String sheetColor){
+        numPillows = 1;
+        this.height = height;
+        this.sheetColor = sheetColor;
+        price = 100;
+    }
+
+    public void setSheetColor(String sheetColor) {
+        this.sheetColor = sheetColor;
+    }
+
 
 
 }
