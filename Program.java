396369b0b07851f79267afecc12464bae67e0e70
rys4590 <ronnie.yadid@gmail.com>
2025-08-03 17:51:53 +0300

    Question6

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program {
+
+    public static void executeStudent(){
+        Student Ronnie = new Student("Ronnie",9,5);
+        System.out.println(Ronnie);
+        if(Ronnie.isHighSchool())
+            System.out.println("Ronnie is in highschool!");
+        else
+            System.out.println("Ronnie ain't in highschool just yet");
+    }
+
+}
