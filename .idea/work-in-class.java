cce5b505fcda573ac6f0f34d8807898bc2db4664
rys4590 <ronnie.yadid@gmail.com>
2025-08-03 16:24:55 +0300

    updateA

---
+++ .idea/work-in-class.java
@@ -0,0 +1,4 @@
+public work-in-class.java
+public static void main(String[] args){
+
+}
---
+++ Bed.java
@@ -0,0 +1,26 @@
+public class Bed {
+
+    private double height;
+    private int numPillows;
+    private String sheetColor;
+
+    public Bed(double height, int numPillows, String sheetColor){
+        this.numPillows = numPillows;
+        this.height = height;
+        this.sheetColor = sheetColor;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getNumPillows() {
+        return numPillows;
+    }
+
+    public String getSheetColor() {
+        return sheetColor;
+    }
+
+
+}
