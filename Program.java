40872c24033498c2fcb6b59828de26bc2ec43c48
rys4590 <ronnie.yadid@gmail.com>
2025-08-10 19:36:58 +0300

    question 1

--- Program.java
+++ Program.java
@@ -1,2 +1,15 @@
 public class Program {
+    public static void  error(){
+         new RuntimeException("my message A");
+        Exception index = new  IndexOutOfBoundsException("my message B");
+        index.getStackTrace();
+        throw new RuntimeException();
+    }
+
+    public static void letsCauseAnError(){
+        int x = 0;
+        int y = 5;
+        int z = y/x;
+    }
+
 }
