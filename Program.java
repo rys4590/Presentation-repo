64d145bfdcec329f9c84f54baa606ae7be0544bc
rys4590 <ronnie.yadid@gmail.com>
2025-08-10 17:40:11 +0300

    fixed your point C

--- Program.java
+++ Program.java
@@ -8,7 +8,7 @@ public class Program {
         for (int i = 1; i < month.returnNum() ; i++) {
             sum+=arr[i-1].getDays();
         }
-        sum+= arr[month.returnNum()].getDays()-day;
+        sum+=day;
         return sum;  //not sure about this one at all, but ill check at the end
     }
 
