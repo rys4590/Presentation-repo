b0c4c12e10cbfe139fd6cfa9f90d7c2881ad268b
rys4590 <ronnie.yadid@gmail.com>
2025-08-10 17:19:24 +0300

    question 8

--- SpongeBobCook.java
+++ SpongeBobCook.java
@@ -1,6 +1,13 @@
 public class SpongeBobCook extends Bobsponge{
+
+    public SpongeBobCook(){
+        super();
+        super.playBasketball();
+    }
+
     @Override
     public void playBasketball() {
         System.out.println("NO TIME FOR BASKETBALL, GOTTA COOK");
+
     }
 }
