5fa455b849846b1a1376872b78c162212d4cd17d
rys4590 <ronnie.yadid@gmail.com>
2025-08-10 19:58:00 +0300

    questiON3

--- Program.java
+++ Program.java
@@ -1,15 +1,46 @@
+import java.util.IllegalFormatException;
+import java.util.Scanner;
 public class Program {
+    public static Scanner input = new Scanner(System.in);
     public static void  error(){
          new RuntimeException("my message A");
         Exception index = new  IndexOutOfBoundsException("my message B");
-        index.getStackTrace();
+        index.getMessage();
         throw new RuntimeException();
     }
 
-    public static void letsCauseAnError(){
-        int x = 0;
-        int y = 5;
-        int z = y/x;
+    public static void trying() {
+        try{
+            int x = 0;
+            int y = 5;
+            int z = y / x;
+        }
+        catch(Exception e){
+            System.out.println("error"+ e.getMessage());
+        }
     }
 
+    public static void question3(){
+        int num;
+        for (int i = 0; i > -1; i++) {
+            System.out.println("enter num");
+            num = input.nextInt();
+            if(num==45)
+                illegalEcx();
+            else if(num==100)
+                exeptionRuntime();
+        }
+    }
+
+    public static void exeptionRuntime(){
+        throw new RuntimeException("you entered 100");
+    }
+
+    public static void illegalEcx(){
+        throw new IllegalArgumentException("you entered 45");
+    }
+
+    public static void main(String[] args){
+        question3();
+    }
 }
