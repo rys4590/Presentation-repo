34e10e0f49912d3a043c2eea18903e1ff7b46220
rys4590 <ronnie.yadid@gmail.com>
2025-08-16 15:59:07 +0300

    correctio- constructor is the first fucntion

--- Bobsponge.java
+++ Bobsponge.java
@@ -1,14 +1,14 @@
 public class Bobsponge extends Fish implements ProffesionalBasketballPlayer{
-    @Override
-    public void goToWork() {
-        System.out.println("YEPPEEE YAY IM GOING TO WORK!!");
-        walk();
-    }
-
     public Bobsponge() {
         this.age = 13;
         this.favoriteFood = "BURGER";
         this.name="SPONGEBOB";
+    }
+
+    @Override
+    public void goToWork() {
+        System.out.println("YEPPEEE YAY IM GOING TO WORK!!");
+        walk();
     }
 
     @Override
