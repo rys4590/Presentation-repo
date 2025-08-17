8dfb3aa064518c9f7762c6e474802d216cf2eeb5
rys4590 <ronnie.yadid@gmail.com>
2025-08-17 17:43:40 +0300

    whats nine? i only know consts

--- Student.java
+++ Student.java
@@ -1,12 +1,12 @@
 
 public record Student(String name,int grade, int classNum){
-
+private static final int AT_HIGH =9;
     public Student(int classNum, String name){
-        this(name,9,classNum);
+        this(name, AT_HIGH,classNum);
     }
 
     public boolean isHighSchool(){
-        return(this.grade>9);
+        return(this.grade>AT_HIGH);
     }
 
     public String toString(){
