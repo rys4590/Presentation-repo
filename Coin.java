920341a383b8c0502d682788f0b76f726b3984b5
rys4590 <ronnie.yadid@gmail.com>
2025-08-17 17:01:12 +0300

    QUESTION 3

--- Coin.java
+++ Coin.java
@@ -1,13 +1,17 @@
 public enum Coin {
     SHEKEL( (amount) -> amount*0.3),
-    DOLLAR(amount ->),
-    EURO(new String()),
-    ST(1);
+    DOLLAR(amount -> amount),
+    EURO(amount->amount*1.15),
+    ST(amount->amount+1);
 
     private ToDollar smth;
 
     Coin(ToDollar smth){
         this.smth = smth;
+    }
+
+    public double toDollar(int amount){
+        return toDollar(amount);
     }
 
 }
