c9ace936b6498fbe833af0b161a64dfef0d2eb5a
rys4590 <ronnie.yadid@gmail.com>
2025-08-17 19:45:11 +0300

    no more blep

--- TaskManager.java
+++ TaskManager.java
@@ -19,7 +19,7 @@ public class TaskManager {
 
     public void printFiltered(TaskFilter filter){
         for (int i = 0; i <tasks.length; i++) {
-            if(blep.didFilter(tasks[i]))
+            if(filter.didFilter(tasks[i]))
                 System.out.println(tasks[i]);
         }
     }
@@ -27,13 +27,13 @@ public class TaskManager {
     public void deleteFiltered(TaskFilter filter){
         int count = 0;
         for (int i = 0; i < tasks.length; i++) {
-            if(!blep.didFilter(tasks[i]))
+            if(!filter.didFilter(tasks[i]))
                 count++;
         }
         Task[] temp = new Task[count];
         int j = 0;
         for (int i = 0; i < tasks.length; i++) {
-            if(blep.didFilter(tasks[i])){
+            if(filter.didFilter(tasks[i])){
                 temp[j] = tasks[i];
                 j++;
             }
@@ -43,7 +43,7 @@ public class TaskManager {
 
     public void raiseFilter(TaskFilter filter){
         for (int i = 0; i <tasks.length; i++) {
-            if(blep.didFilter(tasks[i])){
+            if(filter.didFilter(tasks[i])){
                 int importance = tasks[i].getImportance();
                 tasks[i].setImportance(importance++);
             }
@@ -52,7 +52,7 @@ public class TaskManager {
 
     public void lowerFilter(TaskFilter filter){
         for (int i = 0; i <tasks.length; i++) {
-            if(blep.didFilter(tasks[i])){
+            if(filter.didFilter(tasks[i])){
                 int importance = tasks[i].getImportance();
                 tasks[i].setImportance(importance--);
             }
