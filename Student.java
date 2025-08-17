69493ed5d9e6f31fc775b8414fcc92d63bcded95
rys4590 <ronnie.yadid@gmail.com>
2025-08-17 17:40:23 +0300

    constructor on top

--- Student.java
+++ Student.java
@@ -1,15 +1,16 @@
 
 public record Student(String name,int grade, int classNum){
+
+    public Student(int classNum, String name){
+        this(name,9,classNum);
+    }
+
     public boolean isHighSchool(){
         return(this.grade>9);
     }
 
     public String toString(){
         return "grade:"+this.grade+" name:"+this.name()+" classNumber:" + this.classNum;
-    }
-
-    public Student(int classNum, String name){
-        this(name,9,classNum);
     }
 
 }
